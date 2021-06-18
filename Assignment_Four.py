eafe8c460562e9a648fc4a8b83adc55bb2271d8d
xdaiyan <86119865+xdaiyan@users.noreply.github.com>
2021-06-18 12:04:00 -0400

    Update Assignment_Four.py

--- Assignment_Four.py
+++ Assignment_Four.py
@@ -1,6 +1,6 @@
 names_list = []
-username = input("Please enter a name. Enter 0 to exit.")
+username = input("Please enter a name. Enter 0 to exit. ")
 while username != "0":
   names_list.append(username)
-  username = input("Please enter another name. Enter 0 to exit.")
+  username = input("Please enter another name. Enter 0 to exit. ")
 print(names_list)
